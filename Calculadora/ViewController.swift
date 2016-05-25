33604a6b221ac7f7da2b59e8ae93149fdd86f690
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2016-05-24 21:39:04 -0400

    Adicionado ações dos botões

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -26,20 +26,67 @@ class ViewController: UIViewController {
 
     @IBAction func actionSomar(sender: AnyObject) {
         
+        self.descerTeclado()
+        
+        if let v1 = Int(tfNumero1.text!) {
+            if let v2 = Int(tfNumero2.text!) {
+                let resultado = v1 + v2
+                self.tfResultado.text = "\(resultado)"
+            }
+        }
+        
     }
 
     @IBAction func actionSubtrair(sender: AnyObject) {
+        
+        self.descerTeclado()
+        
+        if let v1 = Int(tfNumero1.text!) {
+            if let v2 = Int(tfNumero2.text!) {
+                let resultado = v1 - v2
+                self.tfResultado.text = "\(resultado)"
+            }
+        }
         
     }
     
     @IBAction func actionMultiplicar(sender: AnyObject) {
         
+        self.descerTeclado()
+        
+        if let v1 = Int(tfNumero1.text!) {
+            if let v2 = Int(tfNumero2.text!) {
+                let resultado = v1 * v2
+                self.tfResultado.text = "\(resultado)"
+            }
+        }
+        
     }
     
     @IBAction func actionDividir(sender: AnyObject) {
         
+        self.descerTeclado()
+        
+        if let v1 = Float(tfNumero1.text!) {
+            if let v2 = Float(tfNumero2.text!) {
+                let resultado = v1 / v2
+                self.tfResultado.text = "\(resultado)"
+            }
+        }
+        
     }
     
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.descerTeclado()
+        
+    }
+    
+    func descerTeclado(){
+        self.tfNumero1.resignFirstResponder()
+        self.tfNumero2.resignFirstResponder()
+        self.tfResultado.resignFirstResponder()
+    }
     
 }
 
